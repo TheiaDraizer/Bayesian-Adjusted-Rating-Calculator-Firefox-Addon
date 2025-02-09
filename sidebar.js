df389e496e24837f65828eede2000b5571be0173
TheiaDraizer <teyamartynenko@gmail.com>
2025-02-09 08:13:29 +0100

    hardcoded m

--- sidebar.js
+++ sidebar.js
@@ -2,18 +2,12 @@ let minRating = 0;
 let maxRating = 10;
 
 function calculateBayesianLowerBound(avgRating, numRatings, globalAvg) {
-    const C = 100;
-    const m = C / Math.sqrt(numRatings);
+    const m = 10;
     const Z = 1.96;
-    const maxRating = 10;
-
     const alpha = avgRating * numRatings + globalAvg * m;
     const beta = (maxRating - avgRating) * numRatings + (maxRating - globalAvg) * m;
-
     const stdDev = Math.sqrt((alpha * beta) / ((alpha + beta) ** 2 * (alpha + beta + 1)));
-
     const lowerBound = (alpha / (alpha + beta)) - (Z * stdDev);
-
     return lowerBound.toFixed(2);
 }
 
